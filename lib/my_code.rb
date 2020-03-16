1c396ef89795985748974dabbca67d9c6fb6984a
Daniel Kersten <dkersten1994@gmail.com>
2020-03-16 22:21:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,13 +1,13 @@
-def map(array)
-  new = []
+def map(source_array)
+  new_array = []
   i = 0
   
-  while i < array.length do
-    new.push(yield(array[i]))
+  while i < source_array.length do
+    new_array.push(yield(source_array[i]))
     
     i += 1
   end
-  new
+  new_array
 end
 
 def reduce(source_array, starting_value=nil)
